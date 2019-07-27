f48c74d10e7ad20376a4b044f445ecd3f15f84ff
Yannis <yannis.samba@gmail.com>
2019-07-28 00:09:23 +0200

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NumericalPasswordInputComponent } from './numerical-password-input/numerical-password-input.component';
+import { PasswordViewerComponent } from './numerical-password-input/password-viewer/password-viewer.component'
+import { RandomizerNumericInputComponent } from './numerical-password-input/randomizer-numeric-input/randomizer-numeric-input.component';
+//material design modules
+import {MatGridListModule,MatToolbarModule, MatButtonModule, MatIconModule, MatListModule, MatDialogModule, MatInputModule } from '@angular/material';
+import {LayoutModule} from '@angular/cdk/layout'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NumericalPasswordInputComponent,
+    PasswordViewerComponent,
+    RandomizerNumericInputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    LayoutModule,
+    MatGridListModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    MatDialogModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
